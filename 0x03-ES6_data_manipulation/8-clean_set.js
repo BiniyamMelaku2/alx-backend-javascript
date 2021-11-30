b4c588d34c201434150e4ff27dcdd69eb77b996c
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2021-11-30 20:37:52 +0300

    returns a string of all the set values

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (!startString || !startString.length || typeof startString !== 'string') return '';
+  if (!startString || typeof startString !== 'string') return '';
+
   let concat = '';
   set.forEach((value) => {
     if (value && value.startsWith(startString)) concat += `${value.slice(startString.length)}-`;
   });
-  return concat;
+  return concat.slice(0, concat.length - 1);
 }
