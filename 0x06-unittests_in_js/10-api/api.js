49d49ecd192af370dbce9f97cdd4fd820ab2be7f
BiniyamMelaku2 <BiniyamMelaku2@gmail.com>
2022-01-28 20:28:24 +0300

    Deep equality & Post integration testing

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -3,32 +3,31 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
+  res.end('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.send(`Payment methods for cart ${req.params.id}`);
+  res.end(`Payment methods for cart ${req.params.id}`);
 });
 
 app.get('/available_payments', (req, res) => {
-  const pay = {
+  const obj = {
     payment_methods: {
       credit_cards: true,
       paypal: false,
     },
   };
-  res.json(pay);
+  res.json(obj);
 });
 
 app.post('/login', (req, res) => {
   const username = req.body.userName;
-  res.send(`Welcome ${username}`);
+  res.end(`Welcome ${username}`);
 });
-
 
 app.listen(port, () => {
   console.log('API available on localhost port 7865');
 });
-
-module.exports = app;
